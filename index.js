312d90f3cecfd1be820e819f42e5d53bbd6ca69b
Justin Wu <justinhwu95@gmail.com>
2019-02-28 21:45:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,8 +10,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object,key,value){
 }
 
 function deleteFromObjectByKey(object,key){
-  Object.assign({},object)
-  return 
+  return Object.assign({},object)
 }
 
 function destructivelyDeleteFromObjectByKey(object,key){
